c7b137fb876a2ad2f0fb9f56eb29a15d6b8f85f2
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-06-15 17:13:30 -0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,9 +49,9 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center">
-          <a href="/cart" className="text-black hover:bg-gray-200 px-3 py-2 rounded-md">
+          <Link a="/cart" className="text-black hover:bg-gray-200 px-3 py-2 rounded-md">
           <img src="https://s3-alpha-sig.figma.com/img/a272/6a7d/ff14f3828457d02e0fd3ff1dac0b1706?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RpfW-Huc5VTeIYLwdwcbmS79iY36syyPjBZGVHvk9f9SVYdDy-87LtBs-O5yeiMotCA~scJXHxuLTiTIjICAFdxa7lK9qZDmPwWl7aC-dzAFX2r695wIbutuZSrKtCm1H5XMUhO0t~p33F0JDhbPusKwrEBwlg-VuBUSIb5GLFRqpT~IhIfrt6s8kYZtuC9H-6oPl9eOJoqNqQBpuno30LD2W7~dCpFlW7VYOXADCmNJl~a5GDRmoV3kdE051pdMj62BHwrv-7bSspB4b~lI0AhBr-qUJ1fjgJSAHBoNi4MWXg6Jze-LjKEFqgMHdG-NZahlaGY-V606Hc95yd-11A__" alt="cart" className="h-[30px]" />
-          </a>
+          </Link>
         </div>
             </div>
 
