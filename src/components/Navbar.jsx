9c9021ca25edd5f4db50a0a21eba78cb1b17cb4d
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-06-15 17:17:04 -0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,9 +35,9 @@ const Navbar = () => {
       
 
         <div className={`z-10 flex justify-center h-[70px] ${showMenu ? 'hidden' : 'flex'}`}>
-          <a href="/">
+          <Link to="/">
             <img src="https://s3-alpha-sig.figma.com/img/ead6/7870/f08b28526bae6d5474faf0a4799a99e4?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MlpWVxlpRk0kpZWPZBWkaNwbIdZWUKeCvDba0RqCmEkmng9cNPmzmNhoTBG8pVIR5ebRsmrywb9~cHI9Jg3n3gfSBhKqnBYs64jy9mEVraC0EHRTwd~QooPYyJ4m31e9iAC7JMl9L0gFhRK8QZlp1ySXS0WrvvD-H3Vux7~FpSryTUlHt0OgjjR7WsokO~vTlHKUJYGmXQ~62Azu-nuk78-dd53oZlvR0pMmcYWsrhkdjZ~ybQIahaRRpcAy7zGUUesNcODJZNx12aQSk2z7YvxQvciWrY7qi84tlNa~Ohu5ww0ueFlfXZraYP~Y~z9UDVZVOrhBwJIe-GLUA3W3CA__" alt="Logo" className="h-[140px]" />
-          </a>
+          </Link>
         </div>
 
 
@@ -49,7 +49,7 @@ const Navbar = () => {
         </div>
 
         <div className="flex items-center">
-          <Link a="/cart" className="text-black hover:bg-gray-200 px-3 py-2 rounded-md">
+          <Link to="/cart" className="text-black hover:bg-gray-200 px-3 py-2 rounded-md">
           <img src="https://s3-alpha-sig.figma.com/img/a272/6a7d/ff14f3828457d02e0fd3ff1dac0b1706?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=RpfW-Huc5VTeIYLwdwcbmS79iY36syyPjBZGVHvk9f9SVYdDy-87LtBs-O5yeiMotCA~scJXHxuLTiTIjICAFdxa7lK9qZDmPwWl7aC-dzAFX2r695wIbutuZSrKtCm1H5XMUhO0t~p33F0JDhbPusKwrEBwlg-VuBUSIb5GLFRqpT~IhIfrt6s8kYZtuC9H-6oPl9eOJoqNqQBpuno30LD2W7~dCpFlW7VYOXADCmNJl~a5GDRmoV3kdE051pdMj62BHwrv-7bSspB4b~lI0AhBr-qUJ1fjgJSAHBoNi4MWXg6Jze-LjKEFqgMHdG-NZahlaGY-V606Hc95yd-11A__" alt="cart" className="h-[30px]" />
           </Link>
         </div>
